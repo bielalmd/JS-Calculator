4df28b088a278a1c67aa3e360129fb109406ddd8
bielalmd <bielalmd@gmail.com>
2023-11-22 21:39:30 -0300

    feat: calculate all probabilities

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
+'use strict'
+
 const display = document.getElementById('display');
 const nums = document.querySelectorAll('[id*=tecla]');
 const operators = document.querySelectorAll('[id*=operador]');
 
 let newNumber = true;
+let operator;
+let previousNumber;
+
+const operatorPending = () => operator !== undefined
+
+const calculate = () => {
+    if (operatorPending()) {
+        const currentNumber = parseFloat(display.textContent);
+        newNumber = true;
+        const res = eval (`${previousNumber}${operator}${currentNumber}`);
+        updateDisplay(res);
+    }
+}
 
 const updateDisplay = (text) => {
     if(newNumber){
@@ -14,12 +29,16 @@ const updateDisplay = (text) => {
 }   
 
 const insertNumber = (event) => updateDisplay(event.target.textContent);
-
 nums.forEach (nums => nums.addEventListener('click', insertNumber));
-
-const selectOperator = () => {
-    newNumber = true
+const selectOperator = (event) => {
+    if (!newNumber) {
+        calculate();
+        newNumber = true;
+        operator = event.target.textContent;
+        previousNumber = parseFloat(display.textContent);
+        console.log(operator);
+    }
 }
-operators.forEach (operators => operators.addEventListener('click', selectOperator));
+operators.forEach((operator) => operator.addEventListener('click', selectOperator));
 
 
